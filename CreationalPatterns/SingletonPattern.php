74abf58a7ffad98ee71cca4d409d19af6399e4ce
Edvig <e.smirnov@rozentalgroup.ru>
2022-04-15 17:26:45 +1000

    Initial commit

---
+++ CreationalPatterns/SingletonPattern.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Patterns;
+
+class SingletonPattern
+{
+    /*
+    Creational pattern
+    Only one globally available instance
+
+    Private constructors
+    Private clone methods
+    Static instance property
+
+    Considered an anti-pattern because it introduces global state
+    Should be used properly
+
+    Used inside such patterns as:
+    Dependency Injection
+    Factories
+    Builders
+    Facades
+    */
+
+
+
+
+}
