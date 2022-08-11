087b1b9bd9dd382e6765e8f8da792dcfd327eb62
smartkiparis <smirnov.edvig@yandex.ru>
2022-08-11 21:19:07 +1000

    singleton

--- CreationalPatterns/SingletonPattern.php
+++ CreationalPatterns/SingletonPattern.php
@@ -20,9 +20,50 @@ class SingletonPattern
     Factories
     Builders
     Facades
+
+    Example in Alerts class
     */
+}
+final class Alerts
+{
+    private array $alerts = [];
 
+    private static Alerts $instance;
 
+    private function __construct()
+    {}
 
+    private function __clone()
+    {}
+
+    public static function getInstance(): self
+    {
+        if (!isset(self::$instance)){
+            self::$instance = new self;
+        }
+        return self::$instance;
+    }
+
+    /**
+     * @param string $type
+     * @param string $message
+     *
+     * @return $this
+     */
+    public function add(string $type, string $message): self
+    {
+        $this->alerts[$type] = $message;
+        return $this;
+    }
+
+    public function get(string $type): array
+    {
+        return $this->alerts[$type] ?? [];
+    }
+
+    public function all(): array
+    {
+        return $this->alerts;
+    }
 
 }
